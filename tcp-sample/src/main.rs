8b0381f10366189c583d4735cbd99a7d6d72116b
kip2 <tqa1no4@gmail.com>
2023-11-26 12:55:47 +0900

    基本のサーバーコードを追加

--- tcp-sample/src/main.rs
+++ tcp-sample/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    println!("Wait for connect:");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
